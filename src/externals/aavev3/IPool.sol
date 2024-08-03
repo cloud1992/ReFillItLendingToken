57834d680f2c12052417b1da169844e8cdfb4406
cloud1992 <arturocollado@ratherlabs.com>
2024-08-02 22:11:23 -0300

    :sparkles: added AaveV3 related files

---
+++ src/externals/aavev3/IPool.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+import {DataTypes} from "./DataTypes.sol";
+
+/**
+ * @title IPool
+ * @author Aave
+ * @notice Defines the basic interface for an Aave Pool.
+ */
+interface IPool {
+    /**
+     * @notice Supplies an `amount` of underlying asset into the reserve, receiving in return overlying aTokens.
+     * - E.g. User supplies 100 USDC and gets in return 100 aUSDC
+     * @param asset The address of the underlying asset to supply
+     * @param amount The amount to be supplied
+     * @param onBehalfOf The address that will receive the aTokens, same as msg.sender if the user
+     *   wants to receive them on his own wallet, or a different address if the beneficiary of aTokens
+     *   is a different wallet
+     * @param referralCode Code used to register the integrator originating the operation, for potential rewards.
+     *   0 if the action is executed directly by the user, without any middle-man
+     */
+    function supply(
+        address asset,
+        uint256 amount,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external;
+
+    /**
+     * @notice Withdraws an `amount` of underlying asset from the reserve, burning the equivalent aTokens owned
+     * E.g. User has 100 aUSDC, calls withdraw() and receives 100 USDC, burning the 100 aUSDC
+     * @param asset The address of the underlying asset to withdraw
+     * @param amount The underlying amount to be withdrawn
+     *   - Send the value type(uint256).max in order to withdraw the whole aToken balance
+     * @param to The address that will receive the underlying, same as msg.sender if the user
+     *   wants to receive it on his own wallet, or a different address if the beneficiary is a
+     *   different wallet
+     * @return The final amount withdrawn
+     */
+    function withdraw(
+        address asset,
+        uint256 amount,
+        address to
+    ) external returns (uint256);
+
+    /**
+     * @notice Returns the user account data across all the reserves
+     * @param user The address of the user
+     * @return totalCollateralBase The total collateral of the user in the base currency used by the price feed
+     * @return totalDebtBase The total debt of the user in the base currency used by the price feed
+     * @return availableBorrowsBase The borrowing power left of the user in the base currency used by the price feed
+     * @return currentLiquidationThreshold The liquidation threshold of the user
+     * @return ltv The loan to value of The user
+     * @return healthFactor The current health factor of the user
+     */
+    function getUserAccountData(
+        address user
+    )
+        external
+        view
+        returns (
+            uint256 totalCollateralBase,
+            uint256 totalDebtBase,
+            uint256 availableBorrowsBase,
+            uint256 currentLiquidationThreshold,
+            uint256 ltv,
+            uint256 healthFactor
+        );
+
+    /**
+     * @notice Returns the configuration of the reserve
+     * @param asset The address of the underlying asset of the reserve
+     * @return The configuration of the reserve
+     */
+    function getConfiguration(
+        address asset
+    ) external view returns (DataTypes.ReserveConfigurationMap memory);
+
+    /**
+     * @notice Returns the configuration of the user across all the reserves
+     * @param user The user address
+     * @return The configuration of the user
+     */
+    function getUserConfiguration(
+        address user
+    ) external view returns (DataTypes.UserConfigurationMap memory);
+
+    /**
+     * @notice Returns the state and configuration of the reserve
+     * @param asset The address of the underlying asset of the reserve
+     * @return The state and configuration data of the reserve
+     */
+    function getReserveData(
+        address asset
+    ) external view returns (DataTypes.ReserveData memory);
+
+    /**
+     * @notice Returns the normalized income of the reserve
+     * @param asset The address of the underlying asset of the reserve
+     * @return The reserve's normalized income
+     */
+    function getReserveNormalizedIncome(
+        address asset
+    ) external view returns (uint256);
+
+    /**
+     * @notice Returns the normalized variable debt per unit of asset
+     * @dev WARNING: This function is intended to be used primarily by the protocol itself to get a
+     * "dynamic" variable index based on time, current stored index and virtual rate at the current
+     * moment (approx. a borrower would get if opening a position). This means that is always used in
+     * combination with variable debt supply/balances.
+     * If using this function externally, consider that is possible to have an increasing normalized
+     * variable debt that is not equivalent to how the variable debt index would be updated in storage
+     * (e.g. only updates with non-zero variable debt supply)
+     * @param asset The address of the underlying asset of the reserve
+     * @return The reserve normalized variable debt
+     */
+    function getReserveNormalizedVariableDebt(
+        address asset
+    ) external view returns (uint256);
+
+    /**
+     * @notice Returns the data of an eMode category
+     * @param id The id of the category
+     * @return The configuration data of the category
+     */
+    function getEModeCategoryData(
+        uint8 id
+    ) external view returns (DataTypes.EModeCategory memory);
+
+    /**
+     * @notice Allows a user to use the protocol in eMode
+     * @param categoryId The id of the category
+     */
+    function setUserEMode(uint8 categoryId) external;
+
+    /**
+     * @notice Returns the eMode the user is using
+     * @param user The address of the user
+     * @return The eMode id
+     */
+    function getUserEMode(address user) external view returns (uint256);
+}
+
+interface IAToken {
+    function balanceOf(address account) external view returns (uint256);
+
+    function scaledBalanceOf(address user) external view returns (uint256);
+}
---
+++ src/externals/aavev3/IWETHGateway.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.0;
+
+interface IWETHGateway {
+    function depositETH(
+        address lendingPool,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external payable;
+
+    function withdrawETH(
+        address lendingPool,
+        uint256 amount,
+        address onBehalfOf
+    ) external;
+}
