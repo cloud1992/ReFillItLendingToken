ac54f90a64ff2a0fead1a1e1023fd3d827979ed2
cloud1992 <arturocollado@ratherlabs.com>
2024-08-03 11:33:13 -0300

    :boom: using ECDSA from OpenZeppeling

--- src/refill-token/ReFillTokenCommons.sol
+++ src/refill-token/ReFillTokenCommons.sol
@@ -7,6 +7,8 @@ import "./ReFillTokenErrors.sol";
 import "./ReFillTokenGetters.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 abstract contract ReFillTokenCommons is
     ReFillTokenGetters,
@@ -16,6 +18,8 @@ abstract contract ReFillTokenCommons is
     ERC20
 {
     using SafeERC20 for IERC20;
+    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
 
     // function to receive underlying token ERC20
     function _receiveUnderlying(
@@ -95,75 +99,11 @@ abstract contract ReFillTokenCommons is
         liquidity = _aToken.balanceOf(address(this));
     }
 
-
-     function _getMessageHash(
-        address _to,
-        uint256 _amount,
-        uint256 _nonce
-    ) private pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_to, _amount, _nonce));
-    }
-
-    function _getEthSignedMessageHash(bytes32 _messageHash)
-        private
-        pure
-        returns (bytes32)
-    {
-        /*
-        Signature is produced by signing a keccak256 hash with the following format:
-        "\x19Ethereum Signed Message\n" + len(msg) + msg
-        */
-        return keccak256(
-            abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
-        );
-    }
-
-    function _recoverSigner(
-        bytes32 _ethSignedMessageHash,
-        bytes memory _signature
-    ) private pure returns (address) {
-        (bytes32 r, bytes32 s, uint8 v) = _splitSignature(_signature);
-
-        return ecrecover(_ethSignedMessageHash, v, r, s);
-    }
-
-    function _splitSignature(bytes memory sig)
-        private
-        pure
-        returns (bytes32 r, bytes32 s, uint8 v)
-    {
-        require(sig.length == 65, "invalid signature length");
-
-        assembly {
-            /*
-            First 32 bytes stores the length of the signature
-
-            add(sig, 32) = pointer of sig + 32
-            effectively, skips first 32 bytes of signature
-
-            mload(p) loads next 32 bytes starting at the memory address p into memory
-            */
-
-            // first 32 bytes, after the length prefix
-            r := mload(add(sig, 32))
-            // second 32 bytes
-            s := mload(add(sig, 64))
-            // final byte (first byte of the next 32 bytes)
-            v := byte(0, mload(add(sig, 96)))
-        }
-
-        // implicitly return (r, s, v)
-    }
-
-    function _verify(
-        address signer,
-        address to,
-        uint256 amount,
-        uint256 nonce,
-        bytes calldata signature
-    ) internal pure returns (bool) {
-        bytes32 messageHash = _getMessageHash(to, amount, nonce);
-        bytes32 ethSignedMessageHash = _getEthSignedMessageHash(messageHash);
-        return _recoverSigner(ethSignedMessageHash, signature) == signer;
+    // internal function to verify signature, here we use Elliptic Curve Digital Signature Algorithm (ECDSA) library
+    // from OpenZeppeling
+    function _isValidSignature(address signer, bytes32 hash, bytes memory signature) internal pure returns (bool) {
+        require(signer != address(0), "Missing Signer Address");
+        bytes32 signedHash = hash.toEthSignedMessageHash();
+        return signedHash.recover(signature) == signer;
     }
 }
